e4c2b70a2c9c69c2ec023775e982a47b4e5978ed
Morten Bøgh <mob@trifork.com>
2015-01-23 19:38:45 +0100

    Initial Xcode project with .gitignore update.

---
+++ ProfileKit/ProfileKit.h
@@ -0,0 +1,19 @@
+//
+//  ProfileKit.h
+//  ProfileKit
+//
+//  Created by Morten Bøgh on 23/01/15.
+//  Copyright (c) 2015 Morten Bøgh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProfileKit.
+FOUNDATION_EXPORT double ProfileKitVersionNumber;
+
+//! Project version string for ProfileKit.
+FOUNDATION_EXPORT const unsigned char ProfileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProfileKit/PublicHeader.h>
+
+
