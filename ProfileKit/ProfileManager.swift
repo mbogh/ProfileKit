3444465f2d7b617daa8595afe0892484b5514f77
Morten Bøgh <mob@trifork.com>
2015-01-27 19:10:00 +0100

    Profile Manager init

---
+++ ProfileKit/ProfileManager.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileManager.swift
+//  ProfileKit
+//
+//  Created by Morten Bøgh on 27/01/15.
+//  Copyright (c) 2015 Morten Bøgh. All rights reserved.
+//
+
+import Foundation
+
+class ProfileManager {
+
+}
---
+++ ProfileKitTests/ProfileManagerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileManagerTests.swift
+//  ProfileKit
+//
+//  Created by Morten Bøgh on 27/01/15.
+//  Copyright (c) 2015 Morten Bøgh. All rights reserved.
+//
+
+import Quick
+import Nimble
+import ProfileKit
+
+class ProfileManagerTests: QuickSpec {
+
+}
